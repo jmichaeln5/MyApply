b301df2b2b057b4110e0faabe94d3a5719e43f42
Jon Norton <jmichaeln5@gmail.com>
2018-03-05 08:37:01 -0500

    Added Devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,15 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+
+################################################# devise begin
+#https://github.com/plataformatec/devise
+config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+#https://github.com/plataformatec/devise
+################################################# devise end
+
+
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
